05f6c29a02ae7073328e71de02155bf046d360d0
Matheus Feijo <mat.feijo2506@gmail.com>
2022-05-11 00:24:56 -0400

    first work with vuejs

---
+++ src/components/Formulario.vue
@@ -0,0 +1,71 @@
+<template>
+<div class="box">
+    <div class="columns">
+        <div class="column is-8" role="form" aria-label="Formulario para criar novas tarefas">
+            <input type="text" class="input" placeholder="qual tarefa voce deseja comecar?" />
+        </div>
+
+        <div class="column">
+            <div class="is-flex is-align-items-center is-justify-content-space-between">
+                <section>
+                    <strong>{{tempoDecorrido}}</strong>
+                </section>
+
+                <button class="button" @click="start">
+                    <span class="icon">
+                        <i class="fas fa-play"></i>
+                    </span>
+                    <span>play</span>
+                </button>
+
+                <button class="button" @click="finish">
+                    <span class="icon">
+                        <i class="fas fa-stop"></i>
+                    </span>
+                    <span>stop</span>
+                </button>
+            </div>
+            
+        </div>
+    </div>
+
+</div>
+</template>
+
+<script lang="ts">
+import {
+    defineComponent
+} from "vue"
+export default defineComponent({
+    name: "FormularioTask",
+    data(){
+        return{
+            tempoEmSegundos:0,
+            cronometro:0
+        }
+    },
+    computed:{
+        tempoDecorrido():string{
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+        }
+    },
+    methods: {
+        start(){
+            this.cronometro = setInterval(()=>{
+                this.tempoEmSegundos +=1;
+            },1000);
+            console.log("inicio");
+        },
+
+        finish(){
+            clearInterval(this.cronometro);
+            console.log("fim");
+        }
+    },
+
+})
+</script>
+
+<style>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "@fortawesome/fontawesome-svg-core"
 
 createApp(App).mount('#app')
